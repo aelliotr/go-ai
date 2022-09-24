54edfff8c286e729b4aad5319d957630acc3e92e
Aaron Russell <aaron.e.russell@gmail.com>
2022-09-24 15:04:05 -0400

    Study and comment on gotypes.py

--- dlgo/gotypes.py
+++ dlgo/gotypes.py
@@ -1,16 +1,19 @@
 import enum
 from collections import namedtuple
 
-
+# Use emum to represent stone colors
 class Player(enum.Enum):
     black = 1
     white = 2
-    
+
+    # class property to switch color
     @property
     def other(self):
         return Player.black if self == Player.white else Player.white
 
+# named tuple to access points as "row" and "col"
 class Point(namedtuple ('Point', 'row col')):
+    # return neighbors
     def neighbors(self):
         return [
             Point(self.row - 1, self.col),
