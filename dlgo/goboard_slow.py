7a749017c40039f14741970c57ff4742d182db70
Aaron Russell <aaron.e.russell@gmail.com>
2022-09-24 15:46:00 -0400

    more typo fixes

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -39,7 +39,7 @@ class Board():
                     adjacent_same_color.append(neighbor_string)
             else:
                 if neighbor_string not in adjacent_opposite_color:
-                    adjacent_opposite_color.append()
+                    adjacent_opposite_color.append(neighbor_string)
         new_string = GoString(player, [point], liberties)
         for same_color_string in adjacent_opposite_color:
             new_string = new_string.merged_with(same_color_string)
